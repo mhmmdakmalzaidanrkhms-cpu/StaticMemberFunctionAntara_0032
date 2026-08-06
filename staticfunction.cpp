17e309219eb8bc9b2e5c2aa69232091cf884c85a
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-06 18:12:54 +0700

    Add Mahasiswa class with static member variable

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string> //menyertakan library string
 using namespace std;
+
+class Mahasiswa {
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+};

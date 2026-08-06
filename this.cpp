3a662686a9cf717b6aa0f0845bd1bedbad86b67d
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-06 18:19:30 +0700

    Menambahkan kelas buku dengan metode setget untuk mengelola variabel judul.

--- this.cpp
+++ this.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+class buku{
+    string judul;
+    public:
+    string setget(string a){
+        this->judul= a; //nilai parameter 'judul' untuk member
+                            // variabel 'judul'
+        return this->judul; // return variabel judul.
+    }
+};
